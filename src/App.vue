bbef4553eada0a410227842f1098f877ed8bc941
susons <kristapsbulssusons@gmail.com>
2026-02-02 02:00:38 +0200

    [slots]

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <TheHeader />
+    <BadgeList />
+    <UserInfo
+      :full-name="activeUser.name"
+      :info-text="activeUser.description"
+      :role="activeUser.role"
+    ></UserInfo>
+  </div>
+</template>
+
+<script>
+import BadgeList from "./components/BadgeList.vue";
+import TheHeader from "./components/TheHeader.vue";
+import UserInfo from "./components/UserInfo.vue";
+
+export default {
+  components: {
+    TheHeader,
+    BadgeList,
+    UserInfo,
+  },
+  data() {
+    return {
+      activeUser: {
+        name: "Maximilian Schwarzmüller",
+        description: "Site owner and admin",
+        role: "admin",
+      },
+    };
+  },
+};
+</script>
+
+<style>
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
---
+++ src/components/UserInfo.vue
@@ -0,0 +1,17 @@
+<template>
+  <base-card>
+    <template #header>
+      <h3>{{ fullName }}</h3>
+      <base-badge :type="role" :caption="role.toUpperCase()"></base-badge>
+    </template>
+    <p>{{ infoText }}</p>
+  </base-card>
+</template>
+
+<script>
+export default {
+  props: ["fullName", "infoText", "role"],
+};
+</script>
+
+<style scoped></style>
