fdedad9b7b5d6b1ffa586077597887d37cb9f14b
susons <kristapsbulssusons@gmail.com>
2026-02-04 00:24:15 +0200

    qweqweqweqw

---
+++ src/components/CourseGoals.vue
@@ -0,0 +1,20 @@
+<template>
+  <ul>
+    <li v-for="goal in goals" :key="goal">
+      <slot :item="goal"></slot>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  components: {},
+  data() {
+    return {
+      goals: ["Learn cooking", "learn carrot cakes"],
+    };
+  },
+};
+</script>
+
+<style></style>
---
+++ src/components/ManageGoals.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <h2>Manage goals</h2>
+    <input ref="goal" type="text" />
+    <button @click="setGoal">Set goal</button>
+    <teleport to="#app">
+      <ErrorAlert v-if="inputError"
+        ><h2>input is invalid</h2>
+        <p>Enter some goal you want to achive</p>
+        <button @click="inputError = false">Close</button></ErrorAlert
+      >
+    </teleport>
+  </div>
+</template>
+
+<script>
+import ErrorAlert from "./ErrorAlert.vue";
+
+export default {
+  components: {
+    ErrorAlert,
+  },
+  data() {
+    return {
+      inputError: false,
+    };
+  },
+  methods: {
+    setGoal() {
+      const value = this.$refs.goal.value;
+
+      if (!value) {
+        this.inputError = true;
+      }
+    },
+  },
+};
+</script>
